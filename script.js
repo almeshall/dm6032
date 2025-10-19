b07fa2fde10e307edf62a282907521b05b6f25d0
almeshall <almeshalmahmood@gmail.com>
2025-10-19 13:10:26 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("submit").onsubmit = function(){
+  alert("thank you for buying");
+return false;
+};
+
+document.getElementByTagName("tr")[1].onclick = function(){
+  alert("Sold out");};
